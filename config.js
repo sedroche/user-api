8906200ed5ac31f3f891a01a6f2a790e9a404c3a
jrochenrfm <john.roche@nearform.com>
2016-09-10 15:36:16 +0100

    Implement read API and test

--- config.js
+++ config.js
@@ -2,9 +2,15 @@
 
 exports.httpResponses = {
     OK: 200,
-    SERVER_ERROR: 500
+    SERVER_ERROR: 500,
+    NOT_FOUND: 404
 };
 
 exports.APP_PORT = 3000;
+
+exports.errorObjects = {
+    NOT_FOUND_ERROR: { error: 'User id is invalid' },
+    SERVER_ERROR: { error: 'Server error' }
+};
 
 exports.MONGO_URL = 'mongodb://localhost:27017/redhat';
