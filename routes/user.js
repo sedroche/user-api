4543f3d6e84117f2a2d6e9c73e62364d03b16e94
jrochenrfm <john.roche@nearform.com>
2016-09-10 10:09:49 +0100

    Add initial crud routes

---
+++ routes/user.js
@@ -0,0 +1,21 @@
+var httpResponses = require('../config').httpResponses;
+
+exports.create = (req, res) => {
+    res.status(httpResponses.OK).send('create');
+};
+
+exports.read = (req, res) => {
+    res.status(httpResponses.OK).send('read');
+};
+
+exports.update = (req, res) => {
+    res.status(httpResponses.OK).send('update');
+};
+
+exports.delete = (req, res) => {
+    res.status(httpResponses.OK).send('delete');
+};
+
+exports.list = (req, res) => {
+    res.status(httpResponses.OK).send('list');
+};
